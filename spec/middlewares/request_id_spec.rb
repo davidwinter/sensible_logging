9aa5633b551e859e94dbf3d663f4a2172c020ddd
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2019-01-08 15:09:02 +0000

    Identify variables not in use, but left as documentataion

--- spec/middlewares/request_id_spec.rb
+++ spec/middlewares/request_id_spec.rb
@@ -6,7 +6,7 @@ describe RequestId do
   it 'adds a request id to the env if one does not already exist' do
     env = {}
 
-    status, headers, body = described_class.new(app).call(env)
+    _status, headers, _body = described_class.new(app).call(env)
 
     expect(env['request_id']).to_not be_nil
     expect(env['request_id']).to eq(headers['X-Request-Id'])
@@ -16,7 +16,7 @@ describe RequestId do
     existing_request_id = '123ABC'
     env = { 'HTTP_X_REQUEST_ID' => existing_request_id }
 
-    status, headers, body = described_class.new(app).call(env)
+    _status, headers, _body = described_class.new(app).call(env)
 
     expect(headers['X-Request-Id']).to eq(existing_request_id)
     expect(env['request_id']).to eq(existing_request_id)
