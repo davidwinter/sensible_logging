f6609738a4ca69f5a671070195acfae1db90d06f
Anthony King <anthony@madetech.com>
2019-01-14 14:49:00 +0000

    remove trailing whitespace

--- lib/sensible_logging/middlewares/tagged_logger.rb
+++ lib/sensible_logging/middlewares/tagged_logger.rb
@@ -10,7 +10,7 @@ class TaggedLogger
     app,
     logger: Logger.new(STDOUT),
     tags: [],
-    use_default_tags: true, 
+    use_default_tags: true,
     tld_length: 1,
     include_log_severity: true
   )
