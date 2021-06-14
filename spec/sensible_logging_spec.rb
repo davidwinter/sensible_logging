145314f8a4ffdfa3250916da124aaf4c46d76bfd
David Winter <i@djw.me>
2021-06-14 11:11:27 +0100

    refactor: split out Rack::CommonLogger test to own file

--- spec/sensible_logging_spec.rb
+++ spec/sensible_logging_spec.rb
@@ -1,24 +1,7 @@
 # frozen_string_literal: true
 
-class MyApp
-  def call(env)
-  end
-end
-
 describe SensibleLogging do
   it 'has a version number' do
     expect(SensibleLogging::VERSION).not_to be nil
-  end
-end
-
-describe Rack::CommonLogger do
-  it 'disables common logger by default' do
-    logger = instance_double('MyLogger', write: false)
-
-    common_logger = Rack::CommonLogger.new(MyApp.new, logger)
-
-    common_logger.call({})
-
-    expect(logger).to_not have_received(:write)
   end
 end
