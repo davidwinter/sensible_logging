524d5aba1e20cbf2d4e1b7b76364305146bb9243
David Winter <i@djw.me>
2021-06-14 11:11:27 +0100

    refactor: split out Rack::CommonLogger test to own file

---
+++ spec/rack/common_logger_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class MyApp
+  def call(env); end
+end
+
+describe Rack::CommonLogger do
+  it 'disables common logger by default' do
+    logger = instance_double('MyLogger', write: false)
+
+    common_logger = described_class.new(MyApp.new, logger)
+
+    common_logger.call({})
+
+    expect(logger).not_to have_received(:write)
+  end
+end
