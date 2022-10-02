2e112c3d5ebaa55ae09bc2fa4a8fee7bf645d2cb
David Winter <i@djw.me>
2022-10-02 09:56:35 +0100

    style: rubocop fixes

--- spec/rack/common_logger_spec.rb
+++ spec/rack/common_logger_spec.rb
@@ -4,9 +4,13 @@ class MyApp
   def call(env); end
 end
 
+class MyLogger
+  def write; end
+end
+
 describe Rack::CommonLogger do
   it 'disables common logger by default' do
-    logger = instance_double('MyLogger', write: false)
+    logger = instance_double(MyLogger, write: false)
 
     common_logger = described_class.new(MyApp.new, logger)
 
--- spec/sensible_logging_spec.rb
+++ spec/sensible_logging_spec.rb
@@ -2,6 +2,6 @@
 
 describe SensibleLogging do
   it 'has a version number' do
-    expect(SensibleLogging::VERSION).not_to be nil
+    expect(SensibleLogging::VERSION).not_to be_nil
   end
 end
