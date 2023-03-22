8a739dda291fff0f2e78988ac7c0616f9cf8df8e
Holger Frohloff <holger@holgerfrohloff.de>
2023-03-22 14:34:19 +0000

    Require complete active_support gem
    According to this comment [1] we need to load the whole of active_support
    and cannot load the tagged_logging class individually.
    
    [1] https://github.com/rails/rails/pull/47733#issuecomment-1479671957

--- lib/sensible_logging/middlewares/tagged_logger.rb
+++ lib/sensible_logging/middlewares/tagged_logger.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'ipaddr'
-require_relative '../../active_support/tagged_logging'
+require 'active_support'
 require_relative '../helpers/subdomain_parser'
 
 # Allow custom tags to be captured
